8e1ef7039fbc87e3210074f01a20dba30a965945
Edison <edison16029@gmail.com>
2021-11-22 18:18:35 +0530

    Aspect Ordering

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,26 +2,14 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(4)
 public class MyDemoLoggingAspect {
-	
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-	private void forDaoPackage() {}
-	
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
-	private void getter() {}
-	
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
-	private void setter() {}
-	
-	@Pointcut("forDaoPackage() && !(getter() || setter())")
-	private void forDaoPackageNoGetterSetter() {}
-
-	// this is where we add all of our related advices for logging
+	// this is where we add all of our related advice for logging
 //	@Before("execution(public void addAccount())")
 //	@Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
 //	@Before("execution(public void add*())")
@@ -32,15 +20,8 @@ public class MyDemoLoggingAspect {
 //	@Before("execution(* add*(..))")
 //	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))") //All class, all method, all params
 //	@Before("forDaoPackage()")
-	@Before("forDaoPackageNoGetterSetter()")
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
-		System.out.println("\n========>>> Executing @Before advice()");
+		System.out.println("\n========>>> Logging some data");
 	}
-	
-//	@Before("forDaoPackage()")
-//	public void performApiAnalytics() {
-//		System.out.println("========>>> Executing @Before advice number two!!");
-//	}
-	
-
 }
